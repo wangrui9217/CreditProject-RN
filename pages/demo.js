02f90d5c37cf74a2e69be564cbadfff12efdc9a8
wangrui <155480015@qq.com>
2019-05-18 11:13:12 +0800

    下拉刷新

--- pages/demo.js
+++ pages/demo.js
@@ -7,18 +7,105 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, TouchableOpacity, FlatList} from 'react-native';
+import {Platform, StyleSheet, Text, View, TouchableOpacity, FlatList, ListView,RefreshControl} from 'react-native';
 import {px} from './util/fix'
+import LoadMoreFooter from './../components/LoadMoreFooter'
 export default class DemoComponent extends Component {
     constructor(props) {
         super(props);
+        var ds = new ListView.DataSource({rowHasChanged:(row1, row2) => row1 !== row2});
         this.state = {
-            refreshing: false,
+            data:['111', '222', '333'],
+            dataSource: ds.cloneWithRows(['111', '222', '333']),
+            isRefreshing: false,
+            isEmptyData: false,
+            isLoadMore: false,
+            isLoadAll: false,
+            extend: '',
+            page: 1
         };
     }
     render() {
         return (
-            <View style={styles.container}></View>
+            <View style={styles.container}>
+                {
+                    this.state.isEmptyData ?
+                        this.isEmptyData():
+                        <ListView
+                        dataSource={this.state.dataSource}
+                        renderRow={rowData => this.renderRow(rowData)}
+                        onEndReached={() => this._toEnd()}
+                        onEndReachedThreshold={10}
+                        // renderFooter={this._toEnd}
+                        refreshControl={
+                            <RefreshControl
+                                refreshing={this.state.isRefreshing}
+                                tintColor="gray"
+                                title="正在刷新"
+                                onRefresh={() => this._onRefresh()}/>}
+                    />
+                }
+                {
+                    this.state.isLoadMore ?
+                        <LoadMoreFooter isLoadAll={this.state.isLoadAll} />
+                        : null
+                }
+            </View>
+        )
+    }
+    renderRow (rowData) {
+        return (
+            <Text style={{lineHeight: 400}}>{rowData}</Text>
+        )
+    }
+    _toEnd(){
+        if (this.state.isRefreshing==false) {
+            this.setState({
+                isLoadMore:true,
+            })
+            // 立即更改属性值 page+1
+            this.state.page = this.state.page + 1
+            // 网络请求数据
+            this.getEduData();
+        }
+    }
+    getEduData () {
+        setTimeout(() => {
+            // 更新数据源
+            let responseData = [this.state.page, this.state.page, this.state.page]
+            var data = this.state.data;
+            if (!responseData || responseData.length==0) {
+                this.setState({
+                    isEmptyData: this.state.isLoadMore ? false : true,
+                    isRefreshing:false,
+                    isLoadMore:false
+                });
+                if (this.state.isLoadMore) {
+                    // 立即更改属性值 page-1
+                    this.state.page = this.state.page - 1
+                }
+            } else {
+                data = this.state.isLoadMore ? [...this.state.data , ...responseData] : responseData
+                this.setState({
+                    data:data,
+                    isRefreshing:false,
+                    dataSource:this.state.dataSource.cloneWithRows(data),
+                    isLoadMore:false,
+                });
+            }
+        }, 3000)
+    }
+    _onRefresh () {
+        this.setState({
+            isRefreshing: true,
+            isLoadMore: false,
+            page: 1
+        })
+        this.getEduData()
+    }
+    isEmptyData () {
+        return (
+            <Text>暂无数据</Text>
         )
     }
 }
